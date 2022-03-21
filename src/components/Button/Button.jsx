40ccb17b5a3ce09d26ebba819f491c5ef350b944
Gustavo Ferreira <gustavo_ferreiracosme03@protonmail.com>
2022-03-21 16:13:11 -0300

    Criados todos os estilos para os botões

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import './button.css'
+
+const STYLES = [
+    "btn--primary--solid",
+    "btn--primary--outline",
+    "btn--light-blue--solid",
+    "btn--light-blue--outline",
+    "btn--red--solid",
+    "btn--red--outline",
+    "btn--white",
+    "btn--white--shadow"
+]
+
+const SIZES = [
+  "btn--large",
+  "btn--medium",
+  "btn--small"
+]
+
+export default function Button({
+    children,
+    type,
+    onClick,
+    buttonStyle,
+    buttonSize
+}) {
+  return (
+    <button className={`btn ${buttonStyle} ${buttonSize}`}onClick={onClick} type={type}>
+        {children}
+    </button>
+  )
+}
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 
+import Test from '../pages/Test'
+
 export default function getRoutes() {
   return (
         <>
-            <Route exact path="/" />
+            <Route exact path="/" element={<Test />}/>
         </>
   )
 }
