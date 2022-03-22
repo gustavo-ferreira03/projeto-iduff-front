1d3d09c99fde333775abc142bc63f5f72047580d
Gustavo Ferreira <gustavo_ferreiracosme03@protonmail.com>
2022-03-22 13:37:11 -0300

    Corrigido o tamanho da white_box

--- src/components/ViewResume/ViewResume.jsx
+++ src/components/ViewResume/ViewResume.jsx
@@ -1,16 +1,49 @@
 import classes from './ViewResume.module.css';
+import Table from '../Table/Table';
+import Button from '../Button/Button';
+
+// Precisa colocar as colunas da tabela
+const columns = [
+    "Nome",
+    "",
+    "Período",
+    "CHT"
+  ]
+  
+  // Carrega o conteúdo que vai ser colocado dentro de cada linha (pode colocar botões ou dropdowns tb)
+  const data = [
+    ["Cálculo 1A",
+    "",
+    "Seg e Qua, 14h as 16h",
+    "401",
+    <Button buttonSize="btn--small" buttonStyle="btn--light-blue--solid">GERENCIAR</Button>],
+    ["Cálculo 2A",
+    "",
+    "Seg e Qua, 14h as 16h",
+    "402",
+    <Button buttonSize="btn--small" buttonStyle="btn--light-blue--solid">GERENCIAR</Button>]
+  ]
 
 export function ViewResume (){
     return(
-        <div className={classes.white_box}>
-            <h1 className={classes.v_C}>Visualizar currículos</h1>
-            <section className={classes.List}>
-                <p className={classes.VizuAqui}>Vizualize aqui o currículo de qualquer um dos cursos da UFF.</p>
-                <div>
-                    <h2 className={classes.Curso}>Curso:</h2>
-                    {/* Menu drop down */}
+        <div className={classes.container}>
+            <div className={classes.white_box}>
+                <div classes={classes.content_container}>
+                    <h1 className={classes.v_C}>Visualizar currículos</h1>
+                    <section className={classes.List}>
+                        <div className={classes.VizuAqui}>
+                            <p>Vizualize aqui o currículo de qualquer um dos cursos da UFF.</p>
+                            <div>
+                                <h2 className={classes.Curso}>Curso:</h2>
+                                {/* Menu drop down */}
+                            </div>
+                        </div>
+                        <div className={classes.Table}>
+                            <Table columns={columns} data={data}/>
+                        </div>
+                    </section>
                 </div>
-            </section>
+            </div>
         </div>
     )
 }
--- src/pages/Test.jsx
+++ src/pages/Test.jsx
@@ -7,19 +7,17 @@ import Button from '../components/Button/Button'
 const columns = [
   "Nome",
   "Período",
-  "CHT"
+  "CHT",
 ]
 
 // Carrega o conteúdo que vai ser colocado dentro de cada linha (pode colocar botões ou dropdowns tb)
 const data = [
   ["Cálculo 1A",
   "Seg e Qua, 14h as 16h",
-  "401",
-  <Button buttonSize="btn--small" buttonStyle="btn--light-blue--solid">GERENCIAR</Button>],
+  "60"],
   ["Cálculo 2A",
   "Seg e Qua, 14h as 16h",
-  "402",
-  <Button buttonSize="btn--small" buttonStyle="btn--light-blue--solid">GERENCIAR</Button>]
+  "60"]
 ]
 
 export default function Test() {
