2e960b4e0466bc04f0558be5f41d96ff15429270
PedroRimes <pcoutinhorimes@gmail.com>
2022-03-22 19:39:18 -0300

    link historico

--- src/components/Sidebar/NavBar/NavBar.jsx
+++ src/components/Sidebar/NavBar/NavBar.jsx
@@ -4,11 +4,15 @@ import styles from './NavBar.module.css'
 import {Link} from "react-router-dom"
 import student_icon from '../assets/student_icon.png'
 import curriculum_icon from '../assets/curriculum_icon.png'
-import subscription_icons from '../assets/subscription_icon.png';
+import subscription_icon from '../assets/subscription_icon.png'
+import history_icon from '../assets/history_icon.png';
 
 export default function NavBar() {
   return (
     <nav className={styles.navbar}>
+        <NavButton
+            img={history_icon}
+        >Histórico</NavButton>
         <Link to="/ViewResume">
         <NavButton 
             img={curriculum_icon}
@@ -18,7 +22,7 @@ export default function NavBar() {
             img={student_icon}
         >Turmas</NavButton>
         <NavButton 
-            img={subscription_icons}
+            img={subscription_icon}
         >Inscrição Online</NavButton>
     </nav>
   )
