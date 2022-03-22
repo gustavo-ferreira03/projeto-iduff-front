4e4ccdf01cd32b2e35f06a8db4f9571da0d0672a
PedroRimes <pcoutinhorimes@gmail.com>
2022-03-21 21:34:50 -0300

    Course adc

---
+++ src/components/ItemList/Course.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import styles from "./Course.module.css"
+
+export function Course(props){
+    <div>
+        <p>{props.name}</p>
+        <p>{props.semester}</p>
+        <p>{props.ch}</p>
+    </div>
+}
+
--- src/pages/Test.jsx
+++ src/pages/Test.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
-import Sidebar from '../components/Sidebar/Sidebar'
+
 
 export default function Test() {
   return (
     <>
+  
     </>
   )
 }
