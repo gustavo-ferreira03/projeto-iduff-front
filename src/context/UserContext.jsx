91e138559845b306df6320212a3ebda23a33fca5
Gustavo Ferreira <gustavo_ferreiracosme03@protonmail.com>
2022-03-23 16:05:24 -0300

    Adicionado arquivo para o context do usuário atual

---
+++ src/context/UserContext.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { useContext, useState, useEffect } from 'react'
+
+const UserContext = React.createContext()
+export default function ThemeProvider() {
+    const [current_user, setCurrentUser] = useState({})
+    useEffect(() => {
+        api.post("/auth/login", data)
+        .then(resp => {
+        localStorage.setItem('token', resp.data.token);
+        })
+        .catch(error => console.error(error.message))
+    })
+
+    return (
+        <ThemeContext.Provider value={user}>
+            {children}
+        </ThemeContext.Provider>
+    )
+}
